cc201c01e66d284f191ad6414497c12e3c5b25d4
local <local@locals-macbook-pro.local>
2017-10-16 16:07:56 -0700

    Initial Commit

---
+++ testrepo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  testrepo
+//
+//  Created by local on 10/16/17.
+//  Copyright © 2017 local. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
